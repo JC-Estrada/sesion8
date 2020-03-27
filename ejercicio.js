791c63c4b609f7080813044e73cf9b1df421182f
JC <JCEstrada>
2020-03-26 22:08:27 -0600

    ok

---
+++ ejercicio.js
@@ -0,0 +1,17 @@
+var nombre = "Juan Carlos";
+var apellido = "Garcia";
+
+var nombreCompleto = nombre + apellido;
+var edad = 28;
+
+var ciudad = "mexico"
+var pasatiempo = "jugar"
+var colorfav = "rojo"
+
+document.getElementById("city").innerHTML = ciudad
+document.getElementById("hobby").innerHTML = pasatiempo
+document.getElementById("favorite color").innerHTML = colorfav
+
+
+document.getElementById("my-name").innerHTML = nombreCompleto;
+document.getElementById("age").innerHTML = edad
---
+++ index.js
@@ -0,0 +1,40 @@
+/* 1. Variables y conexión JS
+- TODAS LAS VARIABLES DEBEN ESTAR EN MINUSCULA*/
+
+var nombre = "Juan Carlos";
+var apellido = "Estrada";
+
+console.log(nombre + apellido);
+
+var edad = 26;
+console.log(edad);
+
+var dobleEdad = edad * 2;
+console.log(dobleEdad);
+
+var tengoPerro = true;
+console.log(tengoPerro);
+
+/*Ejercicio 1
+-Tres "string" ( tres textos)
+- Tres "numeros"
+- Tres "booleanos"
+*/
+
+var ciudad = "mexico";
+var alcaldia = "iztacalco";
+var casa = "blanca";
+console.log(ciudad + alcaldia + casa);
+
+var añosviiendo = 20;
+var habitantes = 4;
+var autos= 3;
+console.log(añosviiendo + habitantes + autos);
+
+var tengoAgua = true;
+var tengovecinosChismoso =  true;
+var megustadondeVivo = true;
+console.log(tengovecinosChismoso);
+
+
+var desayuno = ["Manzanas", "Licuado"]
